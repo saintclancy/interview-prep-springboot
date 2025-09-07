0ddaf6a05a4dba7b6a3672f0dabb8d317b795628
saintclancy <clancylong94@gmail.com>
2025-09-07 12:02:49 -0400

    Created Spring Skeleton with spring starter io, dependencies added: - Data JPA - Validation - Starter Web - Spring Kafka - Spring Boot Devtools - H2 - Lombok - Spring Boot Starter test - Spring Kafka Test

---
+++ flex-pod-task-api/src/main/java/com/clancy/flexpod/taskapi/FlexPodTaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.clancy.flexpod.taskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexPodTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexPodTaskApiApplication.class, args);
+	}
+
+}
---
+++ flex-pod-task-api/src/test/java/com/clancy/flexpod/taskapi/FlexPodTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clancy.flexpod.taskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexPodTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
